60c1c3dad9d905ff33a4356ece0536c23ebc0a24
development <brennopimenta@gmail.com>
2020-11-06 11:10:26 -0200

    Inserindo arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./views/home/home.component";
+import { ClienteCrudComponent } from "./views/cliente-crud/cliente-crud.component";
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+
+const routes: Routes = [
+  {
+  path: "",
+  component: HomeComponent
+  },
+  {
+    path: "clientes",
+    component: ClienteCrudComponent
+  },
+  {
+    path: "clientes/create",
+    component: ClienteCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { from } from 'rxjs';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule} from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ClienteCrudComponent } from './views/cliente-crud/cliente-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ClienteCrudComponent,
+    RedDirective,
+    ForDirective,
+    ClienteCreateComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
